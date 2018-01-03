342772eb2b4f097df6415611d7a60239291584c7
schutzekatze <vladimir.nik94@gmail.com>
2018-01-04 00:11:31 +0100

    Minor fixes :D

--- run-production.sh
+++ run-production.sh
@@ -35,4 +35,4 @@ sudo mkdir -p $NGINX_SERVERS_DIR
 sudo ln -sf $PROJECT_DIR/$MAIN_APP/nginx.conf $NGINX_SERVERS_DIR/$MAIN_APP.conf
 sudo systemctl start nginx
 
-sudo uwsgi --ini $MAIN_APP/uwsgi.ini
+uwsgi --ini $MAIN_APP/uwsgi.ini
