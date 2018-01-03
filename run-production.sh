47dd4c49093aa6064beb2ed23ea85fff1d4c3c27
schutzekatze <vladimir.nik94@gmail.com>
2018-01-03 22:28:41 +0100

    Minor fix

--- run-production.sh
+++ run-production.sh
@@ -31,6 +31,7 @@ done;
 
 sudo python manage.py collectstatic
 
+sudo mkdir -p $NGINX_SERVERS_DIR
 sudo ln -sf $PROJECT_DIR/$MAIN_APP/nginx.conf $NGINX_SERVERS_DIR/$MAIN_APP.conf
 sudo systemctl start nginx
 
