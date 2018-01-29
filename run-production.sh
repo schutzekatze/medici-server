410aeeced797d7b4d07730a88832a0ba6261b42c
schutzekatze <vladimir.nik94@gmail.com>
2018-01-29 05:59:06 +0100

    Minor change

--- run-production.sh
+++ run-production.sh
@@ -5,7 +5,7 @@ touch system.log.1
 
 source config.sh
 
-sudo python manage.py collectstatic
+sudo python manage.py collectstatic --noinput
 
 sudo mkdir -p $NGINX_SERVERS_DIR
 sudo ln -sf $PROJECT_DIR/$MAIN_APP/nginx.conf $NGINX_SERVERS_DIR/$MAIN_APP.conf
